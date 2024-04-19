b8d2f4adae3d02c372403a14560c058317cad316
Willim <wpalacio3@cuc.edu.co>
2024-04-19 10:05:09 -0500

    entregable 01

---
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -0,0 +1,20 @@
+export const Inicio = () => {
+  return (
+    <>
+      <div className="pt-4 d-flex justify-content-center">
+        <div className="col-md-5">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Recent updates</h2>
+            <p>
+              @Username
+              <br />
+              @Username
+              <br />
+              @Username
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
